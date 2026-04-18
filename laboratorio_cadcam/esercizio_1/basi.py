e203fca1070ae8cf4e23a69585282ea28d6df457
Marco Rizzi <rizzi97@hotmail.com>
2026-04-18 11:39:21 +0200

    Risoluzione Esercizio 1.1

--- laboratorio_cadcam/esercizio_1/basi.py
+++ laboratorio_cadcam/esercizio_1/basi.py
@@ -18,10 +18,15 @@ print(f'Peso unitario: {peso_kg} kg')
 print(f'Peso totale: {numero_pezzi * peso_kg:.3f} kg')
 
 # TODO 1: crea una variabile 'materiale' con il valore 'Al6061'
+materiale = 'Al6061'
 # TODO 2: stampa la variabile materiale con un messaggio descrittivo
+print(f'Materiale: {materiale}')
 
 # type() dice di che tipo e' una variabile
 print(f'Tipo di nome_parte: {type(nome_parte)}')
 print(f'Tipo di numero_pezzi: {type(numero_pezzi)}')
 
 # TODO 3: stampa il tipo di peso_kg e di e_conforme
+print(f'Tipo di peso_kg: {type(peso_kg)}')
+print(f'Tipo di materiale: {type(materiale)}')
+print(f'Tipo di e_conforme: {type(e_conforme)}')
